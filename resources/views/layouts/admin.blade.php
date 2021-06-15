814074b93f90d3744c3ceb6a0dabe31f1183adf8
hojinlee <infohojin@gmail.com>
2021-06-15 17:06:58 +0900

    layout component

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Admin Layout</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    @livewireStyles
+</head>
+
+<body>
+    
+    <x-layout>
+        {{--
+        @include("theme.layout")
+        --}}
+    </x-layout>
+    
+    
+    <script src="js/app.js"></script>
+    @livewireScripts
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+use App\Http\Livewire\Admin\AdminMain;
+Route::get('/admin',AdminMain::class);
